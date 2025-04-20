44861f511b7f31ec199bfc09382c164c47dc3c59
Nicolas Berger <n.berger@stud.unibas.ch>
2025-04-20 18:20:53 +0200

    improve readability of decorList.add

--- src/main/java/igoat/client/SceneDecoration.java
+++ src/main/java/igoat/client/SceneDecoration.java
@@ -11,14 +11,38 @@ public class SceneDecoration {
         decorList.add(new Decoration(
             Sprite.class.getResource("/sprites/Wall.png").toExternalForm(),
             20, -44, 1460, 64,
-            true, 0, 0, 64, 64
+            true,
+            0, 0, 64, 64
         ));
 
         // Room 1
-        decorList.add(new Decoration(Sprite.class.getResource("/sprites/closet.png").toExternalForm(), 1300, -10, 64, 64, true, 0, 0, 64, 64));
-        decorList.add(new Decoration(Sprite.class.getResource("/sprites/shelf_with_objects.png").toExternalForm(), 1237, -10, 64, 64, true, 0, 0, 64, 64));
-        decorList.add(new Decoration(Sprite.class.getResource("/sprites/table_dirty.png").toExternalForm(), 1300, 100, 64, 64, true, 0, 0, 64, 64));
-        decorList.add(new Decoration(Sprite.class.getResource("/sprites/jar.png").toExternalForm(), 1300, 70, 64, 64, false, 0, 0, 64, 64));
+        decorList.add(new Decoration(
+            Sprite.class.getResource("/sprites/closet.png").toExternalForm(),
+            1300, -10, 64, 64,
+            true,
+            0, 0, 64, 64
+        ));
+
+        decorList.add(new Decoration(
+            Sprite.class.getResource("/sprites/shelf_with_objects.png").toExternalForm(),
+            1237, -10, 64, 64,
+            true,
+            0, 0, 64, 64
+        ));
+
+        decorList.add(new Decoration(
+            Sprite.class.getResource("/sprites/table_dirty.png").toExternalForm(),
+            1300, 100, 64, 64,
+            true,
+            0, 0, 64, 64
+        ));
+
+        decorList.add(new Decoration(
+            Sprite.class.getResource("/sprites/jar.png").toExternalForm(),
+            1300, 70, 64, 64,
+            false,
+            0, 0, 64, 64
+        ));
 
         return decorList;
     }
