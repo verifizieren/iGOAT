a08f7743e4516e4e913c15029d9b43c24df2b431
Nicolas Berger <n.berger@stud.unibas.ch>
2025-04-20 18:06:42 +0200

    add SceneDecoration for adding pixelart

--- src/main/java/igoat/client/SceneDecoration.java
+++ src/main/java/igoat/client/SceneDecoration.java
@@ -1,4 +1,25 @@
 package igoat.client;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SceneDecoration {
+    public static List<Decoration> getDecorList() {
+        List<Decoration> decorList = new ArrayList<>();
+
+        // Top wall
+        decorList.add(new Decoration(
+            Sprite.class.getResource("/sprites/Wall.png").toExternalForm(),
+            20, -44, 1460, 64,
+            true, 0, 0, 64, 64
+        ));
+
+        // Room 1
+        decorList.add(new Decoration(Sprite.class.getResource("/sprites/closet.png").toExternalForm(), 1300, -10, 64, 64, true, 0, 0, 64, 64));
+        decorList.add(new Decoration(Sprite.class.getResource("/sprites/shelf_with_objects.png").toExternalForm(), 1237, -10, 64, 64, true, 0, 0, 64, 64));
+        decorList.add(new Decoration(Sprite.class.getResource("/sprites/table_dirty.png").toExternalForm(), 1300, 100, 64, 64, true, 0, 0, 64, 64));
+        decorList.add(new Decoration(Sprite.class.getResource("/sprites/jar.png").toExternalForm(), 1300, 70, 64, 64, false, 0, 0, 64, 64));
+
+        return decorList;
+    }
 }
