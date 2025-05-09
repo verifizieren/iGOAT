d75c57a379812f36c51e71c31967e468b902082e
Max Mendes <135263966+verifizieren@users.noreply.github.com>
2025-05-10 00:08:31 +0200

    ready status bug fixed

--- src/main/java/igoat/server/ClientHandler.java
+++ src/main/java/igoat/server/ClientHandler.java
@@ -905,6 +905,11 @@ public class ClientHandler implements Runnable {
         }
 
         joinLobby(lobbyToJoin);
+        // Send ready status of all lobby members to the joining client
+        for (ClientHandler member : lobbyToJoin.getMembers()) {
+            String readyStatusMsg = "ready_status:" + member.getNickname() + "," + member.isReady();
+            sendMessage(readyStatusMsg);
+        }
     }
 
     private void handleNewLobby() {
